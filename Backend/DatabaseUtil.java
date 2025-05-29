fa15b01a522d1063071ff00b60916a650b268656
pravin <peterpravin2506@gmail.com>
2025-05-29 19:12:54 +0530

    modified....

--- Backend/DatabaseUtil.java
+++ Backend/DatabaseUtil.java
@@ -4,21 +4,20 @@ import java.sql.SQLException;
 
 public class DatabaseUtil {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/moviedb";
-<<<<<<< HEAD
-    private static final String DB_USER = "your_db_username";
-    private static final String DB_PASSWORD = "your_db_password";
+
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "2506";
 
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL driver
-=======
-    private static final String DB_USER = "admin";
+
+    private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "2506";
 
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
->>>>>>> 1672aae (completed)
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
